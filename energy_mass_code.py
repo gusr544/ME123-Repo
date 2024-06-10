1f02b1e7564f33aeb5db7f34123ac4be462c56da
gusr544 <70772521+gusr544@users.noreply.github.com>
2024-06-10 12:00:10 -0700

    Update energy_mass_code.py

--- energy_mass_code.py
+++ energy_mass_code.py
@@ -150,14 +150,72 @@ print("Best combination mass", steel_masses24[best_index])
 Se_steel = 100000
 PLA_s = 7250
 
+def calcTorques(i,eff):
+    t1 = (10*9.8* l/ eff) * in_to_m * Nm_to_lbin
+    t2 = 10*9.8*l*i[2]/ (eff*i[1]) * in_to_m * Nm_to_lbin
+    t3 = 10*9.8*l*i[2]/ (eff*i[1]) * in_to_m * Nm_to_lbin
+    t4 = 10*9.8*l*i[2]*i[4]/ (eff*i[1]*i[3]) * in_to_m * Nm_to_lbin
+    t5 = 10*9.8*l*i[2]*i[4]/ (eff*i[1]*i[3]) * in_to_m * Nm_to_lbin
+    t6 = 10*9.8*l*i[2]*i[4] * i[6]/ (eff*i[1]*i[3]* i[5]) * in_to_m * Nm_to_lbin
+    t7 = 10*9.8*l*i[2]*i[4] * i[6]/ (eff*i[1]*i[3]* i[5]) * in_to_m * Nm_to_lbin
+    return t1,t2,t3,t4,t5,t6,t7
+
 def calculateWidths(i, eff,p):
     w1 = gearWidth((10*9.8* l/ eff) * in_to_m * Nm_to_lbin, 2*i[1]*p,Se_steel)
     w2 = gearWidth(10*9.8*l*i[2]/ (eff*i[1]) * in_to_m * Nm_to_lbin,2*i[2]*p, Se_steel)
     w3 = gearWidth(10*9.8*l*i[2]/ (eff*i[1]) * in_to_m * Nm_to_lbin, 2*i[3]*p,Se_steel)
     w4 = gearWidth(10*9.8*l*i[2]*i[4]/ (eff*i[1]*i[3]) * in_to_m * Nm_to_lbin, 2*i[4]*p,Se_steel)
     w5 = gearWidth(10*9.8*l*i[2]*i[4]/ (eff*i[1]*i[3]) * in_to_m * Nm_to_lbin, 2*i[5]*p,Se_steel)
-    return w1,w2,w3,w4,w5
+    w6 = gearWidth(10*9.8*l*i[2]*i[4] * i[6]/ (eff*i[1]*i[3]* i[5]) * in_to_m * Nm_to_lbin, 2*i[6]*p,Se_steel)
+    w7 = gearWidth(10*9.8*l*i[2]*i[4] * i[6]/ (eff*i[1]*i[3]* i[5]) * in_to_m * Nm_to_lbin, 2*i[7]*p,Se_steel)
+    return w1,w2,w3,w4,w5,w6,w7
+
+def calcGearForces(t,r):
+    f1 = t[0]/1.25
+    f2 = f1
+    f3 = t[2]/r[3]
+    f4 = f3
+    f5 = t[4]/r[5]
+    f6 = f5
+    f7= 1*9.8 / 4.448
+    return f1,f2,f3,f4,f5,f6,f7
+
+def calcShaftForces(f):
+    s2 = f[1] - f[2]
+    s3 = f[3] - f[4]
+    s4 = f[5] - f[6]
+    return s2,s3,s4
 
 widths = calculateWidths(valid_combinations[best_index], steel_effs24[best_index], 24)
 
 print(f"Calculated gear widths for the best combination: {widths}")
+
+L = 2.5
+
+def calcTorsion(torques):
+    t1 = torques[2]*2/(np.pi*.125**3)
+    t2 = torques[4]*2/(np.pi*.125**3)
+    t3 = torques[6]*2/(np.pi*.125**3)
+    return t1,t2,t3
+
+def calcBending(loads):
+    b1 = loads[0]*L/(np.pi*0.125**3)
+    b2 = loads[1]*L/(np.pi*0.125**3)
+    b3 = loads[2]*L/(np.pi*0.125**3)
+    return b1,b2,b3
+
+#%%
+torques = calcTorques(best_combination,steel_effs24[best_index])
+forces = calcGearForces(torques, best_combination)
+shafts = calcShaftForces(forces)
+shaftT = calcTorsion(torques)
+shaftB = calcBending(shafts)
+
+print("Torques: ", torques)
+print("Gear Forces: ", forces)
+print("Shaft Forces: ", shafts)
+print("Shaft torsion: ", shaftT)
+print("Shaft bending: ", shaftB)
+
+
+
